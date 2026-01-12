c990a1ba3e4b6fbd3b3eb31b98195ba125d0ee49
Bluebullet07 <brain.k7@gmail.com>
2026-01-12 13:19:55 +0000

    Added second blog and company details

--- js/blog.js
+++ js/blog.js
@@ -7,17 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
   faqItems.forEach(item => {
     const question = item.querySelector('.faq-question');
     
-    question.addEventListener('click', () => {
-      // Close all other items
-      faqItems.forEach(otherItem => {
-        if (otherItem !== item) {
-          otherItem.classList.remove('active');
-        }
+    if (question) {
+      question.addEventListener('click', () => {
+        // Toggle current item
+        item.classList.toggle('active');
       });
-      
-      // Toggle current item
-      item.classList.toggle('active');
-    });
+    }
   });
 
   // Smooth scroll for anchor links
